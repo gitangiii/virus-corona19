6ec46dd7e8b6aabb714585d789aab4e05daac275
anggitanr <anggitanrrr14@gmail.com>
2020-10-01 01:09:48 +0700

    Initial Commit

---
+++ database/migrations/2020_09_15_021507_create_table_petugas.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablePetugas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_petugas', function (Blueprint $table) {
+            $table->id();
+            $table->string(namapetugas);
+            $table->date(tgllahir);
+            $table->string(alamat);
+            $table->string(jabatan);
+            $table->string(nohp);
+            $table->string(username);
+            $table->string(password);
+            $table->string(idlevel)->unsigned();
+            $table->string(photo);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_petugas');
+    }
+}
